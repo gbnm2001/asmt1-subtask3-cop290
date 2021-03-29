9385f2a7fd8eefc2b7ae45e65860d12054f1c048
gbnm2001 <78373993+gbnm2001@users.noreply.github.com>
2021-03-29 19:22:56 +0530

    Update method4.cpp

--- method4.cpp
+++ method4.cpp
@@ -12,6 +12,8 @@ using namespace std;
 using namespace cv;
 
 //make all the required variables as global variables
+//change all the numbers where 7 is written to any number to use that many threads.
+//best result(accuracy,runtime) is obtained for no_threads=2
 int no_threads = 7;
 
 int QD[7];
@@ -25,14 +27,9 @@ Mat back_crop;
 Mat frame;
 Mat frame1;Mat frame2;
 Mat back_image;
-/*Mat warp_frame1;
-Mat warp_frame2;
-Mat warp_frame3;
-Mat warp_frame4;*/
+
 int fm_count=1;
-/*int qd1 =0;int qd2=0;int qd3=0;int qd4=0; //measure of queue density
-int dd1 =0;int dd2=0;int dd3=0;int dd4=0;//measure of dynamic density
-bool t1free = true; bool t2free = true; bool t3free=true; bool t4free=true;*/
+
 bool doing = true;
 ofstream table("m4.txt");
 pthread_mutex_t* mutex1;
@@ -81,9 +78,7 @@ int main(int argc,char* argv[]){
     warpPerspective(back_image, warp_back, h, back_image.size());
     
     back_crop = warp_back(Rect(0, 0, 400, 800));
-    /*int rows = back_crop.rows;
-    cols = back_crop.cols;
-    cout<<rows<<','<<cols<<endl;*/
+
     
     table<< "time , queue density , dynamic density\n";
     string sr="shit\n";
@@ -94,13 +89,11 @@ int main(int argc,char* argv[]){
     pthread_mutex_init(mutex1, NULL); 
     pthread_t threads[no_threads];
 
-    //int IDS[no_threads];
-    int IDs[no_threads];//so that each integer is a different object to pass to the threads
+    int IDs[no_threads];//so that each integer is a different object that are passed to the threads
 
     for(int i=0; i<no_threads; i++){
         IDs[i]=i;
         tfree[i]=true;
-        //cout<<i<<'\n';
         if(0!=pthread_create(&threads[i], NULL, subtractor, &IDs[i])){cout<<"error creating thread1\n";}
     }
 
@@ -110,7 +103,7 @@ int main(int argc,char* argv[]){
     int sub=5*no_threads-2;
 
     while (true){
-        //cout<<"stuck\n";
+       
         bool next = cap.read(frame);
             if (next==false){
                 cout<<"end of video\n";
@@ -125,45 +118,21 @@ int main(int argc,char* argv[]){
                 if(fm_ass_counter == 5*no_threads){ fm_ass_counter=0;}
             }
 
-            //else if(fm_count%10==6){frame2=frame;}
-            //cout<<fm_count<<','<<table_ass_counter<<'\n';
+
             if(fm_count % divider == table_ass_counter){
                 //cout<<"writing\n";
                 if(table_ass_counter == divider-1){
                     table<< fm_count-sub <<','<<QD[0]/320000.0<<','<<DD[0]/320000.0<<'\n';
                     table_ass_counter = 4;
                     fm_count++;
-                    //QD[0]=0;
-                    //DD[0]=0;
+
                     continue;
                 }
                 table<<fm_count - sub<<',' << QD[(table_ass_counter+1)/5]/320000.0 << ','<<DD[(table_ass_counter+1)/5]/320000.0<<'\n';
                 table_ass_counter+=5;
-                //QD[(table_ass_counter+1)/5]=0;
-                //DD[(table_ass_counter+1)/5]=0;
-                //cout<<"stuck\n";
-                //if(table_ass_counter==divider){table_ass_counter=0;}
+
             }
             fm_count++;
-            //cout<<"done\n";
-
-            /*if(fm_count%20==6){//19, do -18 for writing
-            	table<<fm_count-5<<','<<qd1/320000.0<<','<<dd1/320000.0<<'\n';
-            	qd1=0;dd1=0;
-            }
-            else if(fm_count%20==11 && fm_count>10){//4 
-            	table<<fm_count-5<<','<<qd2/320000.0<<','<<dd2/320000.0<<'\n';
-            	qd2=0;dd2=0;
-            }
-            else if(fm_count%20==16){//9
-            	table<<fm_count-5<<','<<qd3/320000.0<<','<<dd3/320000.0<<'\n';
-            	qd3=0;dd3=0;
-            }
-            else if(fm_count%20==1 &fm_count>20){//14
-            	table<<fm_count-5<<','<<qd4/320000.0<<','<<dd4/320000.0<<'\n';
-            	qd4=0;dd4=0;
-            }*/
-
         
     }
     cout<<fm_count<<'\n';
@@ -176,7 +145,7 @@ int main(int argc,char* argv[]){
     cout<<"done\n";
 }   
 
-//common resources - frame, fm_count, table, doing
+//common resources - frames, fm_count, doing
 void* subtractor(void *ID){
     int *Id = (int*) ID;
     int id = *Id;
@@ -188,12 +157,10 @@ void* subtractor(void *ID){
     pBackSub = createBackgroundSubtractorMOG2();
 
     int qd,dd;
-    //cout<<id<<'\n';
-    //double para=0.04*no_threads;
-    //switch((long)id)
+
     while (doing){
         if (fm_count % (5*no_threads) == (5*id+2)){
-            //cout<<"Here"<<fm_count<<'\n';
+
             tfree[id]=false;
             //table<<fm_count-1;
             warpPerspective(frames[id], warp_frame, h, back_image.size());
@@ -223,8 +190,7 @@ void* subtractor(void *ID){
             //pthread_mutex_lock(mutex1);
             //table << ',' << qd/320000.0 << ',' << dd/320000.0 << endl;
             //pthread_mutex_unlock(mutex1);
-            //qd=0;dd=0;
-            //cout<<"t1";
+
             tfree[id]=true;
         }
     }
