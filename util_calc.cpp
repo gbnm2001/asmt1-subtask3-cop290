32f382479fa8a5d5fef1ff30a3bf82091677280a
gbnm2001 <78373993+gbnm2001@users.noreply.github.com>
2021-03-30 23:46:16 +0530

    Update util_calc.cpp

--- util_calc.cpp
+++ util_calc.cpp
@@ -23,3 +23,48 @@ vector<string> split(string str) {
     if (temp != "") { arr.push_back(temp); }
     return arr;
 }
+int main(int argc, char* argv[]){
+   string name = argv[1];
+   ifstream original("outo.txt");
+   ifstream optimised(name);
+   
+   string ol, nl;
+   vector<string> ov, nv;
+   double qdo, ddo, qdn, ddn;
+
+   double qderror, dderror;
+
+   int line_no=0;
+
+   while(getline(original,ol) && getline(optimised,nl)){
+      line_no++;
+      if(line_no == 1){continue;}
+      ov = split(ol);
+      nv = split(nl);
+      qdo = stod ((ov.at(1)));
+      ddo = stod ((ov.at(2)));
+      qdn = stod ((nv.at(1)));
+      ddn = stod ((nv.at(2)));
+      if(qdo==0.0){
+        qderror+=qdn;
+      }else{
+        qderror += pow( (qdo-qdn)/qdo, 2.0);
+      }
+      if (ddo==0.0){dderror+= ddn;}
+      else{
+        dderror += pow( (ddo-ddn)/ddo, 2.0);
+      }
+      /*if(qderror!=0){
+      	cout<<line_no<<','<<qderror<<'\n';
+      }*/
+
+   }
+   //cout<<qderror<<','<<dderror<<','<<line_no-1<<'\n';
+   qderror = pow ( (qderror /(line_no-1)) , 0.5);
+   dderror = pow ( (dderror /(line_no-1)) , 0.5);
+   
+   double qdutil = 1 - qderror;
+   double ddutil = 1 - dderror;
+
+   cout<< "queue density utililty = "<<qdutil*100<<",  "<<"dynamic desity utility = "<<ddutil*100<<'\n';
+}
